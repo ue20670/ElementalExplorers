fbf22d2c4e4dfe0eb161689fa9194b2b500642e3
Alex Elwood <wh20899@bristol.ac.uk>
2023-02-10 11:36:31 +0000

    Added skeleton destruction node

---
+++ Assets/ProceduralPipeline/Nodes/Buildings/DestructionNode.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using XNode;
+
+
+public class DestructionNode : ExtendedNode
+{
+    [Input] public GameObject[] inputBuildings;
+    [Input] public float quantity;
+    [Input] public float scale;
+    [Output] public GameObject[] outputBuildings;
+
+    // Return the correct value of an output port when requested
+    public override object GetValue(NodePort port)
+    {
+
+        if (port.fieldName == "outputBuildings")
+        {
+            return inputBuildings;
+        }
+        return null;
+    }
+
+    public override void CalculateOutputs(Action<bool> callback)
+    {
+        // setup inputs
+        GameObject[] buildings = GetInputValue("buildingData", inputBuildings);
+        float q = GetInputValue("buildingData", quantity);
+        float s = GetInputValue("buildingData", scale);
+
+        // setup outputs
+        List<GameObject> gameObjects = new List<GameObject>();
+
+
+
+        foreach (GameObject building in buildings)
+        {
+            GameObject buildingGO = AddBuildingDestruction(building, q, s);
+            gameObjects.Add(buildingGO);
+        }
+
+        outputBuildings = gameObjects.ToArray();
+        callback.Invoke(true);
+    }
+
+    private GameObject AddBuildingDestruction(GameObject building, float q, float s)
+    {
+        return building;
+    }
+}
