7d6af68476a87a65f05c3fcf8c5fafea17616bdd
George Wigley <georgewigley123@gmail.com>
2023-02-27 05:02:58 +0000

    Now working with MCUT DLL, still need to write a wrapper

--- Assets/Scripts/PluginsTest/pluginTest.cs
+++ Assets/Scripts/PluginsTest/pluginTest.cs
@@ -6,14 +6,12 @@ using UnityEngine;
 public class pluginTest : MonoBehaviour
 {
     // Import and expose native c++ functions
-    [DllImport("TESTPLUGIN", EntryPoint = "SimpleReturn")] public static extern int SimpleReturn();
-    // [DllImport("testUnityPlugin", EntryPoint = "getRandom")] public static extern int getRandom();
-    // [DllImport("testUnityPlugin", EntryPoint = "displaySum")] public static extern int displaySum();
+    [DllImport("mcut-wrapper", EntryPoint = "TestDLLWorking")] public static extern int TestDLLWorking();
     
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(SimpleReturn());
+        Debug.Log(TestDLLWorking());
     }
 
 }
