e076507d32ec97d9de0fde4f98a1409e00b42d9d
Imran Zamin Ali <ge20118@bristol.ac.uk>
2023-03-17 16:56:59 +0000

    added player model and have ik working for arms. need to use vr to finish off head.

---
+++ Assets/Scripts/Player/VRRig.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform rigTarget;
+
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void Map()
+    {
+        rigTarget.position = vrTarget.TransformPoint((trackingPositionOffset));
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+
+public class VRRig : MonoBehaviour
+{
+
+    [SerializeField] private Transform headConstriant;
+
+    [SerializeField] private Vector3 headBodyOffset;
+
+    [SerializeField] private VRMap head;
+    [SerializeField] private VRMap leftHand;
+    [SerializeField] private VRMap rightHand;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        headBodyOffset = transform.position - headConstriant.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = headConstriant.position + headBodyOffset;
+        transform.forward = Vector3.ProjectOnPlane(headConstriant.up, Vector3.up).normalized;
+        head.Map();
+        leftHand.Map();
+        rightHand.Map();
+    }
+}
